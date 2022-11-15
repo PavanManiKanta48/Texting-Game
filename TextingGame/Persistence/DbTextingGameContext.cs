bb99492e763e8739f166004b386143ec47bcd397
Anshika0219 <anshikaa@moback.com>
2022-11-15 22:54:00 +0530

    Registration_Task

--- TextingGame/Persistence/DbTextingGameContext.cs
+++ TextingGame/Persistence/DbTextingGameContext.cs
@@ -17,6 +17,7 @@ public partial class DbTextingGameContext : DbContext
     }
 
     public virtual DbSet<TblUserDetail> TblUserDetails { get; set; }
+    public object TblUserDetail { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
---
+++ TextingGame/Service/Interface/IuserDetail.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interface
+{
+   public interface IuserDetail
+    {
+        List<TblUserDetail> GetUser();
+        bool Register(TblUserDetail tblUser);
+        bool UpdateUserDetail(TblUserDetail tblUser);
+        bool DeleteUserDetail(TblUserDetail tblUser);
+    }
+}
