89cfa31a3f30efe5fa84709e8d772ff2072f26de
Anshika0219 <anshikaa@moback.com>
2022-12-07 18:13:51 +0530

    I have Created Meesage ,Room and UserRoom Model Request Body

---
+++ TextingGame/Domain/Messagemodel/createUserRoomRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Messagemodel
+{
+    public class createUserRoomRequestModel
+    {
+        public int? RoomId { get; set; }
+        public string Message { get; set; } = null!;
+    }
+}
---
+++ TextingGame/Domain/UserRoomModel/ListUserRoomRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.UserRoomModel
+{
+    public class ListUserRoomRequestModel
+    {
+        public int? RoomId { get; set; }
+    }
+}
