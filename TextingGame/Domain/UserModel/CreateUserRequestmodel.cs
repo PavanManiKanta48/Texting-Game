467a14fe55e0dfdd2d4a093d701b36bedd914b91
PavanManiKanta48 <pavanmanikanta484@gmail.com>
2022-12-07 17:34:44 +0530

    Created responce classes for User Module

--- TextingGame/Domain/UserModel/CreateUserRequestmodel.cs
+++ TextingGame/Domain/UserModel/CreateUserRequestmodel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Domain.UserModel
 {
-    public class PostUserRequestmodel
+    public class CreateUserRequestmodel
     {
         public string? UserName { get; set; }
 
--- TextingGame/Domain/UserModel/LoginResponse.cs
+++ TextingGame/Domain/UserModel/LoginResponse.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Domain.UserModel
 {
-    public class UserLoginResponse
+    public class UserLoginResponse : BaseResponseModel
     {
+        public LoginUserRequestModel LoginUserRequest { get; set; } 
     }
 }
