fe42ff7b7cce54f763889b81380bd626f347a13c
Premcharan Kamagari <premcharan.kamagari@kornferry.com>
2022-12-12 17:53:30 +0530

    Enabling user session

---
+++ TextingGame/Domain/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Constants
+    {
+        //Session variables
+        public const string UserId = "UserId";
+    }
+}
